e74857683c2f526421e2f7b9e4450de8763aee9f
oyugirachel <oyugirachel20@gmail.com>
2020-07-27 21:20:41 +0300

    add login activity layout

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.firstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,8 +13,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         tvRegister.setOnClickListener{
-            val intent=Intent(baseContext,Registrationactivity::class.java)
+            val intent= Intent(baseContext,Registrationactivity::class.java)
             startActivity(intent)
+        }
+        btnLogin.setOnClickListener {
+            var UserName=etUserName.text.toString()
+            var Password= etPassword.text.toString()
+            Toast.makeText(baseContext,Password,Toast.LENGTH_LONG).show()
+
+
         }
     }
 }
--- app/src/main/java/com/example/firstapp/Registrationactivity.kt
+++ app/src/main/java/com/example/firstapp/Registrationactivity.kt
@@ -2,11 +2,26 @@ package com.example.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.etPassword
+import kotlinx.android.synthetic.main.activity_main.tvRegister
+import kotlinx.android.synthetic.main.activity_registrationactivity.*
 
 class Registrationactivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrationactivity)
+        tvRegister.setOnClickListener {
+            var FirstName=etFirstName.text.toString()
+            var Password= etPassword.text.toString()
+            var LastName=etLastName.text.toString()
+            var PhoneNumber=etPhoneNumber.text.toString()
+            var ConfirmPassword=etPassword.text.toString()
+            Toast.makeText(baseContext,Password, Toast.LENGTH_LONG).show()
+
+
+        }
     }
 }
